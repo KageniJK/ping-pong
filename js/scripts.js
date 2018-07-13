9b579fd799661fbfecef13ab48d58a1bd7ad7170
James Kageni <james.kageni@gmail.com>
2018-07-13 14:22:52 +0300

    completed back end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,39 @@
 // Front end
-$(document).ready(function(){
+$(document).ready(function() {
 
-$(".input form").submit(function(event){
-  event.preventDefault();
-  var number = parseInt($("input#num").val());
-  $("ul.output").text("<li>"+"test"+"</li>");
+  $(".input form").submit(function(event) {
+    event.preventDefault();
+    var number = parseInt($("input#num").val());
+    
+    //$(".output").text("test");
 
-});
+  });
 
 });
 
 
 // Back end Logic
-var result;
-function pingpong(x){
-  for (i=1;i<=x;i++){
-    if(i%3===0){
-      result = "ping";
+var result =[];
+function pingpong(x) {
+  for (i = 1; i <= x; i++) {
+    if (i % 3 === 0) {
+      if (i % 15 === 0) {
+        //console.log("pingpong")
+        result.push("pingPong");
+        //return result
+      } else {
+        //console.log("ping")
+        result.push("ping");
+        //return result;
+      }
+    } else if (i % 5 === 0) {
+      //console.log("pong")
+      result.push("pong");
       //return result;
-    }
-    else if (i%5===0){
-      result = "pong";
-      //return result;
-    }
-    else if (i%3===0 && num%5===0){
-      result = "pingPong";
-      //return result
-    }
-    else {
-      result = i;
+    } else {
+      //console.log(i);
+      result.push(i);
     };
-
   }
-  return result;
+
 }
