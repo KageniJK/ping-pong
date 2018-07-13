526b2726c2c869a580558752cf97eff072369300
James Kageni <james.kageni@gmail.com>
2018-07-13 11:52:57 +0300

    added the user input and the structure for the back end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,36 @@
 // Front end
 $(document).ready(function(){
 
+$(".input form").submit(function(event){
+  event.preventDefault();
+  var number = parseInt($("input#num").val());
+  $("ul.output").text("<li>"+"test"+"</li>");
+
 });
+
+});
+
+
+// Back end Logic
+var result;
+function pingpong(x){
+  for (i=1;i<=x;i++){
+    if(i%3===0){
+      result = "ping";
+      //return result;
+    }
+    else if (i%5===0){
+      result = "pong";
+      //return result;
+    }
+    else if (i%3===0 && num%5===0){
+      result = "pingPong";
+      //return result
+    }
+    else {
+      result = i;
+    };
+
+  }
+  return result;
+}
