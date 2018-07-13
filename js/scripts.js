d6bdf2c91f8c3ea0d897992734cd268182745564
James Kageni <james.kageni@gmail.com>
2018-07-13 17:40:36 +0300

    removed commented out code on javascript

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,9 @@ $(document).ready(function() {
     event.preventDefault();
     var number = parseInt($("input#num").val());
     pingpong(number);
-    results.forEach(function(result){
-      $("ul#outList").append("<li>"+result+"</li>");
+    results.forEach(function(result) {
+      $("ul#outList").append("<li>" + result + "</li>");
     });
-
-
-    //$(".output").text("test");
 
   });
 
@@ -18,28 +15,20 @@ $(document).ready(function() {
 
 
 // Back end Logic
-var results =[];
+var results = [];
+
 function pingpong(x) {
   for (i = 1; i <= x; i++) {
     if (i % 3 === 0) {
       if (i % 15 === 0) {
-        //console.log("pingpong")
         results.push("Ping-Pong");
-        //return result
       } else {
-        //console.log("ping")
         results.push("Ping");
-        //return result;
       }
     } else if (i % 5 === 0) {
-      //console.log("pong")
       results.push("Pong");
-      //return result;
     } else {
-      //console.log(i);
       results.push(i);
     };
-
   }
-
 }
