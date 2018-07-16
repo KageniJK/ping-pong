d2456ce3003d9c1ee3c43a90e8ea8c7347e3297b
James Kageni <james.kageni@gmail.com>
2018-07-16 11:19:04 +0300

    added functionality to test if the input is a positive integer

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,11 @@ $(document).ready(function() {
     event.preventDefault();
     var number = parseInt($("input#num").val());
     $(".outList").empty();
+    if (Number.isInteger(number) && number>0){
     pingPong(number);
+  } else {
+    alert("Please enter a positive number");
+  }
   });
 });
 
@@ -14,7 +18,7 @@ function pingPong(x) {
   for (i = 1; i <= x; i++) {
     if (i % 3 === 0) {
       if (i % 5 === 0) {
-        $("ul.outList").append("<li>" + "ping-pong" + "</li>");;
+        $("ul.outList").append("<li>" + "ping-pong" + "</li>");
       } else {
         $("ul.outList").append("<li>" + "ping" + "</li>");
       }
